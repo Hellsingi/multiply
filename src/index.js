bf6abf3f0aa11b9b0bda50598e408c9239212b76
Hellsingi <f.c.realmadrid@mail.ru>
2019-10-01 12:28:01 +0300

    solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function multiply(first, second) {
   // your solution
+  let firstNumber = BigInt(first);
+  let secondNumber = BigInt(second);
+
+  let multiplyStr = (firstNumber * secondNumber)+ '';
+  return multiplyStr;
 }
